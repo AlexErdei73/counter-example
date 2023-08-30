552038ddd0e708715345f87e168dcf66e88c9f37
Alex Erdei <mralexerdei@yahoo.co.uk>
2023-08-30 10:18:13 +0100

    Show both fields in view of counter

--- index.js
+++ index.js
@@ -10,19 +10,21 @@ const newState = JSON.parse(JSON.stringify(state));
 const counter = (function counterFactory(counterState, newCounterState) {
   const component = document.querySelector("#counter");
   const counterValue = component.querySelector("#value");
-  const counterIncrement = component.querySelector("#increment");
+  const counterIncrementInput = component.querySelector("#increment-input");
+  const counterIncrementHeading = component.querySelector("#increment-heading");
 
   function init() {
     const counterButton = component.querySelector("button");
 
-    counterValue.textContent = counterState.value;
-    counterIncrement.value = counterState.increment;
+    counterValue.textContent = `Value: ${counterState.value}`;
+    counterIncrementInput.value = counterState.increment;
+    counterIncrementHeading.textContent = `Increment: ${counterState.increment}`;
 
     counterButton.addEventListener("click", function () {
       newCounterState.value = counterState.value + counterState.increment;
     });
 
-    counterIncrement.addEventListener("change", function (event) {
+    counterIncrementInput.addEventListener("change", function (event) {
       const newIncrement = +event.target.value;
       if (!newIncrement) {
         newCounterState.increment = 0;
@@ -32,15 +34,24 @@ const counter = (function counterFactory(counterState, newCounterState) {
     });
   }
 
-  function renderKey(key, property) {
-    if (counterState[key] === newCounterState[key]) return;
+  function renderKey(key) {
+    if (counterState[key] === newCounterState[key]) return false;
     counterState[key] = newCounterState[key];
-    component.querySelector(`#${key}`)[property] = counterState[key];
+    return true;
   }
 
   const render = {
-    value: () => renderKey("value", "textContent"),
-    increment: () => renderKey("increment", "value"),
+    value: () => {
+      if (renderKey("value", "textContent")) {
+        counterValue.textContent = `Value: ${counterState.value}`;
+      }
+    },
+    increment: () => {
+      if (renderKey("increment", "value")) {
+        counterIncrementInput.value = counterState.increment;
+        counterIncrementHeading.textContent = `Increment: ${counterState.increment}`;
+      }
+    },
   };
 
   return {
